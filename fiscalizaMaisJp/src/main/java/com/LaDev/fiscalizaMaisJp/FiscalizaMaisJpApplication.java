34b3f5595dccdeef32651d0269536efa928734bd
Lucas Aragão <lucasaragao@MacBook-Air-de-Lucas.local>
2025-08-11 01:40:54 -0300

    initial commit

---
+++ fiscalizaMaisJp/src/main/java/com/LaDev/fiscalizaMaisJp/FiscalizaMaisJpApplication.java
@@ -0,0 +1,13 @@
+package com.LaDev.fiscalizaMaisJp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalizaMaisJpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalizaMaisJpApplication.class, args);
+	}
+
+}
---
+++ fiscalizaMaisJp/src/test/java/com/LaDev/fiscalizaMaisJp/FiscalizaMaisJpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaDev.fiscalizaMaisJp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalizaMaisJpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
